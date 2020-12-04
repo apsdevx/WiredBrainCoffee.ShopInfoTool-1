cd9292f3686e9d7a427b2d63f389f63276336884
apsdevx <apsdevx@outlook.com>
2020-12-04 14:49:17 +0530

    Added data access project to solution

---
+++ WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WiredBrainCoffee.DataAccess.Model;
+
+namespace WiredBrainCoffee.DataAccess
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShops()
+        {
+            yield return new CoffeeShop { BeansInStock = 1, Location = "Rajasthan" };
+            yield return new CoffeeShop { BeansInStock = 5, Location = "UP" };
+            yield return new CoffeeShop { BeansInStock = 10, Location = "MP" };
+    }
+}
